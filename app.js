31465fba7ec96ec02c0edc10e16af8f1d9fd00be
Honeyposh <odedoyinoyinloluwa@gmail.com>
2023-09-07 15:13:50 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/firsttask/api", (req, res) => {
+  const slackname = req.query.slack_name;
+  const track = req.query.track;
+  const utc_time = new Date();
+  const statusCode = res.statusCode;
+  let daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let dayToday = daysOfTheWeek[utc_time.getDay()];
+
+  let githubrepourl = "https://github.com/honeyposh/First-Task.git";
+  // let githubfileurl=
+
+  const data = {
+    slack_name: slackname,
+    current_day: dayToday,
+    utc_time: utc_time,
+    track: track,
+    status_code: statusCode,
+    // github_file_url:githubfileurl
+    github_repo_url: githubrepourl,
+  };
+  res.json(data);
+});
+app.listen(PORT);
